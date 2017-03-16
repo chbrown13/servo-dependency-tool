81fca34a583325b52edf85284f42b97f0f9d01af
Paaaatrick <patrick_yudh@hotmail.com>
2017-03-15 21:25:09 -0400

    not sure if it's the right way to handle the exceptions in gitpython

--- sample_test/local_repo.py
+++ sample_test/local_repo.py
@@ -1,16 +1,31 @@
 import git
 from git import Repo, Remote
+import traceback
 
 
-def commit(path):
-    repo = Repo(path)
-    print(repo.git.status())
-    print(repo.git.add("."))
-    print(repo.git.commit(m='my commit message'))
-    print(repo.git.push())
+def push(path):
+    try:
+        repo = Repo(path)
+        print(repo.git.status())
+        repo.git.checkout("master")
+        print(repo.git.add("."))
+        print(repo.git.commit(m='version update'))
+        print(repo.git.push())
+    except Exception:
+        traceback.print_exc()
 
 
 def pull(path):
-    repo = git.Repo(path)
-    origin = repo.remotes.origin
-    origin.pull()
+    try:
+        repo = git.Repo(path)
+        origin = repo.remotes.origin
+        s = origin.pull()
+        print(s)
+    except Exception:
+        traceback.print_exc()
+
+
+#PATH = "D:\Dev\Gitpython"
+#pull(PATH)
+
+# push(PATH)
