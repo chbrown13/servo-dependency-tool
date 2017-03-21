c3092fe6a71b4725a44f75866b509fbc6d8cbf8a
Bradford Ingersoll <bingers@ncsu.edu>
2017-03-21 09:18:50 -0400

    Added note about specifying version upon ambiguity.

--- servo_dependency_tool.py
+++ servo_dependency_tool.py
@@ -15,10 +15,10 @@ def run_cargo_update(pkg):
     else:  # Otherwise use default cargo update command
         cargo_bin_path = os.path.expanduser('~/.cargo/bin/cargo')
         args = [cargo_bin_path, 'update', '-p', pkg.name]
+    print(args)
     cmd_out = None
     cmd_err = None
     cmd_out, cmd_err = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    print(args)
     print(cmd_err.decode('utf-8'))
     if 'is ambiguous.' in cmd_err.decode('utf-8'):  # If failure due to ambiguity, use precise version
         if os.path.isfile('mach'):
@@ -27,10 +27,11 @@ def run_cargo_update(pkg):
         else:  # Otherwise use default cargo update command
             cargo_bin_path = os.path.expanduser('~/.cargo/bin/cargo')
             args = [cargo_bin_path, 'update', '-p', (pkg.name + ':' + pkg.version)]
+        print('Specifying version %s...' % pkg.version)
+        print(args)
         cmd_out = None
         cmd_err = None
         cmd_out, cmd_err = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-        print(args)
         print(cmd_err.decode('utf-8'))
 
 
