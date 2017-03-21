9e093cdf83352d350a61f4bcb1644d96b1fd16a3
Bradford Ingersoll <bingers@ncsu.edu>
2017-03-21 13:49:08 -0400

    Fixed mach path

--- servo_dependency_tool.py
+++ servo_dependency_tool.py
@@ -12,7 +12,7 @@ import repo_management
 def run_cargo_update(pkg):
     print("Running update for %s" % pkg.name)
     if os.path.isfile(os.path.join(git_path, 'mach')):
-        mach_path = './mach'
+        mach_path = git_path + '/mach'
         args = [mach_path, 'cargo-update', '-p', pkg.name]
     else:  # Otherwise use default cargo update command
         cargo_bin_path = os.path.expanduser('~/.cargo/bin/cargo')
@@ -25,7 +25,7 @@ def run_cargo_update(pkg):
     print(cmd_err.decode('utf-8'))
     if 'is ambiguous.' in cmd_err.decode('utf-8'):  # If failure due to ambiguity, use precise version
         if os.path.isfile(os.path.join(git_path, 'mach')):
-            mach_path = './mach'
+            mach_path = git_path + '/mach'
             args = [mach_path, 'cargo-update', '-p', (pkg.name + ':' + pkg.version)]
         else:  # Otherwise use default cargo update command
             cargo_bin_path = os.path.expanduser('~/.cargo/bin/cargo')
