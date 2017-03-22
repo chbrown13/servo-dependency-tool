fcbb3511501818512f6a5a797989de6365380535
Chris <dcbrown06@gmail.com>
2017-03-22 15:53:59 -0400

    Renamed servo_test to test and starting unittest for cargo_toml_updater

---
+++ test/README.md
@@ -0,0 +1,3 @@
+## Servo Dependency Tool Unittests
+
+Run: python3 -m unittest \<module-name> (without ".py")
---
+++ test/test_cargo_toml_updater.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+import sys
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0,path)
+import cargo_toml_updater as updater
+from cargo_lock_parser import lock_file_parse
+
+class TestCargoTOMLUpdater(unittest.TestCase):
+
+	def setUp(self):
+		self.lock_file = lock_file_parse("Cargo.lock")
+		self.lock_file.packages["servo"].upgrade_available = True
+		self.lock_file.packages["toml"].upgrade_available = True
+		self.lock_file.packages["rustc-serialize"].upgrade_available = True
+
+		with open("Cargo.toml",'r') as f:
+			self.original = f.read()
+
+	def test_toml_file_update(self):
+		updater.toml_file_update("Cargo.toml",self.lock_file)
+		
