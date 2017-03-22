1937db144c0cbd75c035519a3cb0116c81f3beac
Chris <dcbrown06@gmail.com>
2017-03-21 22:51:38 -0400

    Updating setup.py with gitpython and github3

--- setup.py
+++ setup.py
@@ -1,1 +1,7 @@
 from distutils.core import setup
+
+setup(
+  name='servo-dependency-tool',
+  description='Tool for automatically upgrading Cargo dependencies.',
+  install_requires=['gitpython','github3.py'],
+)
